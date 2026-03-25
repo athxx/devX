8248b521e8e8ade714d5775908adbcb75adbd77f
Athan <athanseal@gmail.com>
2026-03-25 21:01:24 +1300

    split chunks

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,29 @@ export default defineConfig({
       output: {
         entryFileNames: "assets/[name].js",
         chunkFileNames: "assets/chunks/[name].js",
-        assetFileNames: "assets/[name][extname]"
+        assetFileNames: "assets/[name][extname]",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("@codemirror") || id.includes("@lezer")) {
+              return "vendor-codemirror"
+            }
+            if (id.includes("@xterm") || id.includes("xterm")) {
+              return "vendor-xterm"
+            }
+            if (id.includes("solid-js")) {
+              return "vendor-solid"
+            }
+          }
+          if (id.includes("features/db/")) {
+            return "feature-db"
+          }
+          if (id.includes("features/ssh/")) {
+            return "feature-ssh"
+          }
+          if (id.includes("features/rest/")) {
+            return "feature-rest"
+          }
+        }
       }
     }
   }
